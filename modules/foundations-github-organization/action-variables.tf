b48c5d6c12161375b1158161d100d44718b2a927
Ben Zarboni <ben.zarboni@focisolutions.com>
2024-03-06 14:35:36 -0500

    Correct previous fix

--- modules/foundations-github-organization/action-variables.tf
+++ modules/foundations-github-organization/action-variables.tf
@@ -9,17 +9,17 @@ resource "github_actions_secret" "organization_workload_identity_sa" {
 resource "github_actions_variable" "gcp_secret_manager_project_id" {
   provider = github.foundation_org_scoped
 
-  repository      = github_repository.organizations_repo.name
-  variable_name   = "GCP_SECRET_MANAGER_PROJECT"
-  plaintext_value = var.gcp_project_id
+  repository    = github_repository.organizations_repo.name
+  variable_name = "GCP_SECRET_MANAGER_PROJECT"
+  value         = var.gcp_project_id
 }
 
 resource "github_actions_organization_secret" "workload_identity_provider" {
   provider = github.foundation_org_scoped
 
-  secret_name = "WORKLOAD_IDENTITY_PROVIDER"
-  value         = var.workload_identity_provider_name
-  visibility    = "selected"
+  secret_name     = "WORKLOAD_IDENTITY_PROVIDER"
+  plaintext_value = var.workload_identity_provider_name
+  visibility      = "selected"
   selected_repository_ids = [
     github_repository.bootstrap_repo.repo_id,
     github_repository.organizations_repo.repo_id
