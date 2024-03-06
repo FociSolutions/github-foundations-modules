d245e357cd58df96d54d4c978bf3d58ba76e7fa8
Ben Zarboni <ben.zarboni@focisolutions.com>
2024-03-06 16:58:42 -0500

    Overwrite on commit

--- modules/foundations-github-organization/repo_readme.tf
+++ modules/foundations-github-organization/repo_readme.tf
@@ -4,9 +4,10 @@ data "local_file" "main_readme" {
 
 resource "github_repository_file" "main_readme" {
   # Only create this when the readme filename is not empty
-  count      = var.readme_path != "" ? 1 : 0
-  repository = github_repository.organizations_repo.name
-  file       = "README.md"
-  content    = data.local_file.main_readme.content
-  depends_on = [github_repository.organizations_repo]
+  count               = var.readme_path != "" ? 1 : 0
+  repository          = github_repository.organizations_repo.name
+  file                = "README.md"
+  content             = data.local_file.main_readme.content
+  depends_on          = [github_repository.organizations_repo]
+  overwrite_on_create = true
 }
