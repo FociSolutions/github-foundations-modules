30b7493d80570edfc39c31385c37292ee5d86bb9
Tyler Mizuyabu <tylermizuyabu@gmail.com>
2024-03-07 11:54:23 -0500

    making use of module again

--- modules/organization/secrets.tf
+++ modules/organization/secrets.tf
@@ -1,74 +1,7 @@
-locals {
-  # If no selected repositories are set then sets the field to an empty list
-  sanitized_action_secrets = merge(
-    var.actions_secrets,
-    {
-      for k, v in var.actions_secrets : k => {
-        encrypted_value       = v.encrypted_value
-        visibility            = v.visibility
-        selected_repositories = coalesce(v.selected_repositories, [])
-      } if v.visibility == "selected"
-    }
-  )
-  sanitized_codespace_secrets = merge(
-    var.codespaces_secrets,
-    {
-      for k, v in var.codespaces_secrets : k => {
-        encrypted_value       = v.encrypted_value
-        visibility            = v.visibility
-        selected_repositories = coalesce(v.selected_repositories, [])
-      } if v.visibility == "selected"
-    }
-  )
-  sanitized_dependabot_secrets = merge(
-    var.dependabot_secrets,
-    {
-      for k, v in var.dependabot_secrets : k => {
-        encrypted_value       = v.encrypted_value
-        visibility            = v.visibility
-        selected_repositories = coalesce(v.selected_repositories, [])
-      } if v.visibility == "selected"
-    }
-  )
+module "organization_secrets" {
+  source = "../organization_secrets"
 
-
-  all_selected_repositories = {
-    for repo in concat(
-      flatten([for secret in values(var.actions_secrets) : secret.selected_repositories if secret.visibility == "selected" && secret.selected_repositories != null]),
-      flatten([for secret in values(var.codespaces_secrets) : secret.selected_repositories if secret.visibility == "selected" && secret.selected_repositories != null]),
-      flatten([for secret in values(var.dependabot_secrets) : secret.selected_repositories if secret.visibility == "selected" && secret.selected_repositories != null])
-    ) : repo => repo
-  }
+  organization_action_secrets     = var.actions_secrets
+  organization_codespace_secrets  = var.codespaces_secrets
+  organization_dependabot_secrets = var.dependabot_secrets
 }
-
-data "github_repository" "selected_repositories" {
-  for_each = local.all_selected_repositories
-  name     = each.value
-}
-
-resource "github_actions_organization_secret" "action_secret" {
-  for_each = local.sanitized_action_secrets
-
-  secret_name             = each.key
-  encrypted_value         = each.value.encrypted_value
-  visibility              = each.value.visibility
-  selected_repository_ids = [for repo in toset(each.value.selected_repositories) : data.github_repository.selected_repositories["${repo}"].repo_id]
-}
-
-resource "github_codespaces_organization_secret" "codespace_secret" {
-  for_each = local.sanitized_codespace_secrets
-
-  secret_name             = each.key
-  encrypted_value         = each.value.encrypted_value
-  visibility              = each.value.visibility
-  selected_repository_ids = [for repo in toset(each.value.selected_repositories) : data.github_repository.selected_repositories["${repo}"].repo_id]
-}
-
-resource "github_dependabot_organization_secret" "dependabot_secret" {
-  for_each                = local.sanitized_dependabot_secrets
-  secret_name             = each.key
-  encrypted_value         = each.value.encrypted_value
-  visibility              = each.value.visibility
-  selected_repository_ids = [for repo in toset(each.value.selected_repositories) : data.github_repository.selected_repositories["${repo}"].repo_id]
-}
-
--- modules/organization_secrets/secrets.tf
+++ modules/organization_secrets/secrets.tf
@@ -1,16 +1,16 @@
 locals {
   # If no selected repositories are set then sets the field to an empty list
-  sanitized_organization_action_secrets = merge(
+  sanitized_action_secrets = merge(
     var.organization_action_secrets,
     {
-      for k, v in var.organization_action_secrets : k => {
+      for k, v in var.var.organization_action_secrets : k => {
         encrypted_value       = v.encrypted_value
         visibility            = v.visibility
         selected_repositories = coalesce(v.selected_repositories, [])
       } if v.visibility == "selected"
     }
   )
-  sanitized_organization_codespace_secrets = merge(
+  sanitized_codespace_secrets = merge(
     var.organization_codespace_secrets,
     {
       for k, v in var.organization_codespace_secrets : k => {
@@ -20,7 +20,7 @@ locals {
       } if v.visibility == "selected"
     }
   )
-  sanitized_organization_dependabot_secrets = merge(
+  sanitized_dependabot_secrets = merge(
     var.organization_dependabot_secrets,
     {
       for k, v in var.organization_dependabot_secrets : k => {
@@ -30,31 +30,45 @@ locals {
       } if v.visibility == "selected"
     }
   )
+
+
+  all_selected_repositories = {
+    for repo in concat(
+      flatten([for secret in values(var.var.organization_action_secrets) : secret.selected_repositories if secret.visibility == "selected" && secret.selected_repositories != null]),
+      flatten([for secret in values(var.organization_codespace_secrets) : secret.selected_repositories if secret.visibility == "selected" && secret.selected_repositories != null]),
+      flatten([for secret in values(var.organization_dependabot_secrets) : secret.selected_repositories if secret.visibility == "selected" && secret.selected_repositories != null])
+    ) : repo => repo
+  }
+}
+
+data "github_repository" "selected_repositories" {
+  for_each = local.all_selected_repositories
+  name     = each.value
 }
 
 resource "github_actions_organization_secret" "action_secret" {
-  for_each = local.sanitized_organization_action_secrets
+  for_each = local.sanitized_action_secrets
 
   secret_name             = each.key
   encrypted_value         = each.value.encrypted_value
   visibility              = each.value.visibility
-  selected_repository_ids = each.value.selected_repositories
+  selected_repository_ids = [for repo in toset(each.value.selected_repositories) : data.github_repository.selected_repositories["${repo}"].repo_id]
 }
 
 resource "github_codespaces_organization_secret" "codespace_secret" {
-  for_each = local.sanitized_organization_codespace_secrets
+  for_each = local.sanitized_codespace_secrets
 
   secret_name             = each.key
   encrypted_value         = each.value.encrypted_value
   visibility              = each.value.visibility
-  selected_repository_ids = each.value.selected_repositories
+  selected_repository_ids = [for repo in toset(each.value.selected_repositories) : data.github_repository.selected_repositories["${repo}"].repo_id]
 }
 
 resource "github_dependabot_organization_secret" "dependabot_secret" {
-  for_each                = local.sanitized_organization_dependabot_secrets
+  for_each                = local.sanitized_dependabot_secrets
   secret_name             = each.key
   encrypted_value         = each.value.encrypted_value
   visibility              = each.value.visibility
-  selected_repository_ids = each.value.selected_repositories
+  selected_repository_ids = [for repo in toset(each.value.selected_repositories) : data.github_repository.selected_repositories["${repo}"].repo_id]
 }
 
