549733b13ee0e0d5b14f79c93b4634e80649de9e
Ben Zarboni <ben.zarboni@focisolutions.com>
2024-03-07 14:38:17 -0500

    fix: previous missing

--- modules/repository_base/environments.tf
+++ modules/repository_base/environments.tf
@@ -1,5 +1,5 @@
 resource "github_repository_environment" "environemnt" {
-  for_each    = keys(toset(var.environments))
+  for_each    = toset(keys(var.environments))
   repository  = github_repository.repository.name
   environment = each.value
 }
