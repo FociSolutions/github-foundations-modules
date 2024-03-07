855880204bdceb7b739499113871053e31b81088
Tyler Mizuyabu <tylermizuyabu@gmail.com>
2024-03-07 11:30:03 -0500

    deleted id output

--- modules/repository_set/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "repository_ids" {
-  value = merge({
-    for repository_name in keys(var.var.public_repositories) : repository_name => module.public_repositories["${repository_name}"].id
-  }, {
-    for repository_name in keys(var.var.private_repositories) : repository_name => module.private_repositories["${repository_name}"].id
-  })
-  description = "A map of github repository names to ids"
-}
