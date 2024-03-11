3c8e824cd936bc454a38b22aec10fe8c414b15c7
TylerMizuyabu <tyler.mizuyabu@focisolutions.com>
2024-03-11 14:35:57 -0400

    Removed github app datasource

--- modules/organization/rulesets.tf
+++ modules/organization/rulesets.tf
@@ -5,17 +5,23 @@ locals {
     } if ruleset_config.bypass_actors != null && ruleset_config.bypass_actors.teams != null
   ]...)
 
-  branch_ruleset_app_bypassers = merge([
-    for ruleset, ruleset_config in var.branch_rulesets : {
-      for app_slug in ruleset_config.bypass_actors.integrations : "${ruleset}:${app_slug}" => app_slug
-    } if ruleset_config.bypass_actors != null && ruleset_config.bypass_actors.integrations != null
-  ]...)
-
   branch_ruleset_admin_bypassers = merge([
     for ruleset, ruleset_config in var.branch_rulesets : {
       for user in ruleset_config.bypass_actors.organization_admins : "${ruleset}:${user}" => user
     } if ruleset_config.bypass_actors != null && ruleset_config.bypass_actors.organization_admins != null
   ]...)
+
+  branch_ruleset_custom_repository_roles = merge([
+    for ruleset, ruleset_config in var.branch_rulesets : {
+      for role in ruleset_config.bypass_actors.repository_roles : "${ruleset}:${role}" => role
+    } if ruleset_config.bypass_actors != null && ruleset_config.bypass_actors.repository_roles != null && !contains(keys(github_base_role_ids), role)
+  ]...)
+
+  github_base_role_ids = {
+    "maintain" = 2
+    "write" = 4
+    "admin" = 5
+  }
 }
 
 
@@ -26,16 +32,17 @@ data "github_team" "branch_ruleset_bypasser" {
   summary_only = true
 }
 
-data "github_app" "branch_ruleset_bypasser" {
-  for_each = local.branch_ruleset_app_bypassers
-
-  slug = each.value
-}
-
 data "github_user" "branch_ruleset_bypasser" {
   for_each = local.branch_ruleset_admin_bypassers
 
   username = each.value
+}
+
+#github_organization_custom_role is actualy repository custom roles. The provider doesn't seem to support custom github organization roles
+data "github_organization_custom_role" "branch_ruleset_bypasser" {
+    for_each = local.branch_ruleset_custom_repository_roles
+
+    name = each.value
 }
 
 resource "github_organization_ruleset" "branch_ruleset" {
@@ -49,7 +56,7 @@ resource "github_organization_ruleset" "branch_ruleset" {
     for_each = each.value.bypass_actors != null ? toset(coalesce(each.value.bypass_actors.repository_roles, [])) : []
 
     content {
-      actor_id    = bypass_actors.value
+      actor_id    = lookup(github_base_role_ids, bypass_actors.value, data.github_organization_custom_role.branch_ruleset_bypasser["${each.key}:${bypass_actors.value}"])
       actor_type  = "RepositoryRole"
       bypass_mode = "always"
     }
@@ -73,7 +80,7 @@ resource "github_organization_ruleset" "branch_ruleset" {
     for_each = each.value.bypass_actors != null ? toset(coalesce(each.value.bypass_actors.integrations, [])) : []
 
     content {
-      actor_id    = data.github_app.branch_ruleset_bypasser["${each.key}:${bypass_actors.value}"].node_id
+      actor_id    = bypass_actors.value
       actor_type  = "Integration"
       bypass_mode = "always"
     }
