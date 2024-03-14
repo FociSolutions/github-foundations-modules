f941a1240cf196e9246610662dd9301b8ad0f827
Tyler Mizuyabu <tylermizuyabu@gmail.com>
2024-03-14 11:51:56 -0400

    updated repo ruleset

--- modules/repository_base/rulesets.tf
+++ modules/repository_base/rulesets.tf
@@ -1,21 +1,24 @@
 locals {
-  ruleset_team_bypassers = merge([
-    for ruleset, ruleset_config in var.rulesets : {
-      for bypasser in coalesce(ruleset_config.bypass_actors.teams, []) : "${ruleset}:${bypasser.team}" => bypasser.team
-    } if ruleset_config.bypass_actors != null
-  ]...)
+  all_team_bypassers = toset(concat(
+    coalesce(try(var.default_branch_protection_rulesets.bypass_actors.teams, []), []),
+    [
+      for _, ruleset_config in var.rulesets : coalesce(try(ruleset_config.bypass_actors.teams, []), [])
+    ]...
+  ))
 
-  ruleset_admin_bypassers = merge([
-    for ruleset, ruleset_config in var.rulesets : {
-      for bypasser in coalesce(ruleset_config.bypass_actors.organization_admins, []) : "${ruleset}:${bypasser.user}" => bypasser.user
-    } if ruleset_config.bypass_actors != null
-  ]...)
+  all_admin_bypassers = toset(concat(
+    coalesce(try(var.default_branch_protection_rulesets.bypass_actors.organization_admins, []), []),
+    [
+      for _, ruleset_config in var.rulesets : coalesce(try(ruleset_config.bypass_actors.organization_admins, []), [])
+    ]...
+  ))
 
-  ruleset_custom_repository_roles = merge([
-    for ruleset, ruleset_config in var.rulesets : {
-      for bypasser in coalesce(ruleset_config.bypass_actors.repository_roles, []) : "${ruleset}:${bypasser.role}" => bypasser.role if !contains(keys(local.github_base_role_ids), bypasser.role)
-    } if ruleset_config.bypass_actors != null 
-  ]...)
+  all_repository_roles_bypassers = toset(concat(
+    coalesce(try(var.default_branch_protection_rulesets.bypass_actors.repository_roles, []), []),
+    [
+      for _, ruleset_config in var.rulesets : coalesce(try(ruleset_config.bypass_actors.repository_roles, []), [])
+    ]...
+  ))
 
   github_base_role_ids = {
     "maintain" = 2
@@ -26,21 +29,27 @@ locals {
 
 
 data "github_team" "branch_ruleset_bypasser" {
-  for_each = local.ruleset_team_bypassers
+  for_each = {
+    for bypasser in local.all_team_bypassers : bypasser.team => bypasser.team
+  }
 
   slug         = each.value
   summary_only = true
 }
 
 data "github_user" "branch_ruleset_bypasser" {
-  for_each = local.ruleset_admin_bypassers
+  for_each = {
+    for bypasser in local.all_admin_bypassers: bypasser.user => bypasser.user
+  }
 
   username = each.value
 }
 
 #github_organization_custom_role is actualy repository custom roles. The provider doesn't seem to support custom github organization roles
 data "github_organization_custom_role" "branch_ruleset_bypasser" {
-  for_each = local.ruleset_custom_repository_roles
+  for_each = {
+    for bypasser in local.all_repository_roles_bypassers : bypasser.role => bypasser.role
+  }
 
   name = each.value
 }
@@ -56,7 +65,7 @@ resource "github_repository_ruleset" "ruleset" {
     for_each = each.value.bypass_actors != null ? toset(coalesce(each.value.bypass_actors.repository_roles, [])) : []
 
     content {
-      actor_id    = lookup(local.github_base_role_ids, bypass_actors.value.role, data.github_organization_custom_role.branch_ruleset_bypasser["${each.key}:${bypass_actors.value.role}"].id)
+      actor_id    = lookup(local.github_base_role_ids, bypass_actors.value.role, data.github_organization_custom_role.branch_ruleset_bypasser["${bypass_actors.value.role}"].id)
       actor_type  = "RepositoryRole"
       bypass_mode = coalesce(bypass_actors.value.always_bypass, false) ? "always" : "pull_request"
     }
@@ -168,7 +177,7 @@ resource "github_repository_ruleset" "ruleset" {
     for_each = each.value.bypass_actors != null ? toset(coalesce(each.value.bypass_actors.teams, [])) : []
 
     content {
-      actor_id    = data.github_team.branch_ruleset_bypasser["${each.key}:${bypass_actors.value.team}"].id
+      actor_id    = data.github_team.branch_ruleset_bypasser["${bypass_actors.value.team}"].id
       actor_type  = "Team"
       bypass_mode = coalesce(bypass_actors.value.always_bypass, false) ? "always" : "pull_request"
     }
@@ -188,7 +197,7 @@ resource "github_repository_ruleset" "ruleset" {
     for_each = each.value.bypass_actors != null ? toset(coalesce(each.value.bypass_actors.organization_admins, [])) : []
 
     content {
-      actor_id    = data.github_user.branch_ruleset_bypasser["${each.key}:${bypass_actors.value.user}"].id
+      actor_id    = data.github_user.branch_ruleset_bypasser["${bypass_actors.value.user}"].id
       actor_type  = "OrganizationAdmin"
       bypass_mode = coalesce(bypass_actors.value.always_bypass, false) ? "always" : "pull_request"
     }
