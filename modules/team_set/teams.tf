5b262fb6545df3df09770b396c01b8d07ae186f7
Tyler Mizuyabu <tylermizuyabu@gmail.com>
2024-03-25 13:59:50 -0400

    removed preexisting teams variable

--- modules/team_set/teams.tf
+++ modules/team_set/teams.tf
@@ -1,26 +1,20 @@
+locals {
+  preexisting_teams = {
+    for team_name, team_config in var.teams : team_name => team_config if coalesce(team_config.import, false)
+  }
+}
+
+import {
+  for_each = local.preexisting_teams
+
+  to = module.team[each.key].github_team.team
+  id = each.key
+}
+
 module "team" {
   source = "../team"
 
   for_each = var.teams
-
-  team_maintainers = each.value.maintainers
-  team_members     = each.value.members
-  team_description = each.value.description
-  privacy          = each.value.privacy
-  parent_id        = each.value.parent_id
-  team_name        = each.key
-}
-
-import {
-  for_each = var.preexisting_teams
-
-  to = module.prexisting_team[each.key].github_team.team
-  id = each.key
-}
-
-module "prexisting_team" {
-  source   = "../team"
-  for_each = var.preexisting_teams
 
   team_maintainers = each.value.maintainers
   team_members     = each.value.members
--- modules/team_set/variables.tf
+++ modules/team_set/variables.tf
@@ -5,18 +5,7 @@ variable "teams" {
     maintainers = list(string)
     members     = list(string)
     parent_id   = string
+    import      = optional(bool)
   }))
   description = "A map of teams to create where the key is the team name and the value is the configuration. If the team does not have a parent team, the parent_id should be an empty string."
-}
-
-variable "preexisting_teams" {
-  type = map(object({
-    description = string
-    privacy     = string
-    maintainers = list(string)
-    members     = list(string)
-    parent_id   = string
-  }))
-  description = "A map of existing teams where the key is the team name and the value is the configuration. If the team does not have a parent team, the parent_id should be an empty string."
-  default     = {}
 }
