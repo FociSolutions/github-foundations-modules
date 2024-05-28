44a230341ad7f479cb5a52a5cfffcbf2bd89f28a
Tyler Mizuyabu <tylermizuyabu@gmail.com>
2024-05-28 15:06:50 -0400

    fixed variable validation

--- modules/github-foundations/variables.tf
+++ modules/github-foundations/variables.tf
@@ -73,7 +73,7 @@ variable "oidc_configuration" {
     }))
   })
   validation {
-    condition     = var.oidc_configuration.gcp != null || var.oidc_configuration.custom != null
+    condition     = var.oidc_configuration.gcp != null || var.oidc_configuration.custom != null || var.oidc_configuration.azure != null
     error_message = "At least one oidc_configuration must be set."
   }
 }
