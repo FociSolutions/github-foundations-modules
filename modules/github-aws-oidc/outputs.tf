16b75048bae9571e07b98a9d071bf7b61717869c
Tyler Mizuyabu <tylermizuyabu@gmail.com>
2024-06-17 09:41:02 -0400

    added output for role ARN

--- modules/github-aws-oidc/outputs.tf
+++ modules/github-aws-oidc/outputs.tf
@@ -12,3 +12,8 @@ output "dynamodb_table_name" {
   description = "The name of the dynamodb table that was created to store lock file ids."
   value = aws_dynamodb_table.state_lock_table.name
 }
+
+output "organizations_runner_role" {
+  description = "The ARN of the role that the github action runner should assume for the organizations repo"
+  value = aws_iam_role.organizations_role.arn
+}
