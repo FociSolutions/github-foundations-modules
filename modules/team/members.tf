d2aa4449655b26e3969e99488568ca13d4c6162b
vedantthapa <thapavedant@gmail.com>
2025-10-01 10:48:55 -0300

    feat: combine github_team_membership resource blocks into one

--- modules/team/members.tf
+++ modules/team/members.tf
@@ -1,17 +1,16 @@
 locals {
   team_id = local.create_team ? github_team.team[0].id : var.team_id
+  
+  # Combine team_maintainers and team_members into a single map with respective roles
+  memberships = merge(
+    { for username in var.team_maintainers : username => "maintainer" },
+    { for username in var.team_members : username => "member" }
+  )
 }
 
-resource "github_team_membership" "maintainers" {
-  for_each = toset(var.team_maintainers)
+resource "github_team_membership" "memberships" {
+  for_each = local.memberships
   team_id  = local.team_id
-  username = each.value
-  role     = "maintainer"
-}
-
-resource "github_team_membership" "members" {
-  for_each = toset(var.team_members)
-  team_id  = local.team_id
-  username = each.value
-  role     = "member"
+  username = each.key
+  role     = each.value
 }
